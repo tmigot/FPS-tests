6d4c45cc2a69f9265ea41a7799baccb3ccaa3f88
tmigot <tangi.migot@gmail.com>
2021-12-04 00:22:59 +0100

    add benchmark with percival and fps

---
+++ cutest_benchmark_percival_ipopt/benchmark.jl
@@ -0,0 +1,61 @@
+using Pkg; Pkg.activate("")
+using CUTEst, NLPModels, SolverBenchmark
+using NLPModelsIpopt, DCISolver, Percival, FletcherPenaltyNLPSolver
+
+problems = readlines("list_problems.dat")
+cutest_problems = (CUTEstModel(p) for p in problems)
+
+max_time = 1200.0 #20 minutes
+tol = 1e-5
+
+solvers = Dict(
+  :ipopt => nlp -> ipopt(
+      nlp,
+      print_level = 0,
+      dual_inf_tol = Inf,
+      constr_viol_tol = Inf,
+      compl_inf_tol = Inf,
+      acceptable_iter = 0,
+      max_cpu_time = max_time,
+      tol = tol,
+  ),
+  :dcildl => nlp -> dci(
+      nlp, # uses x0 = nlp.meta.x0 by default
+      linear_solver = :ldlfact,
+      max_time = max_time,
+      max_iter = typemax(Int64),
+      max_eval = typemax(Int64),
+      atol = tol,
+      ctol = tol,
+      rtol = tol,
+  ),
+  :percival => nlp -> percival(
+    nlp,
+    #μ::Real = T(10.0),
+    max_iter = typemax(Int64),
+    max_time = max_time,
+    max_eval = typemax(Int64),
+    atol = tol,
+    ctol = tol,
+    rtol = tol,
+    #subsolver_logger::AbstractLogger = NullLogger(),
+    #inity = nothing,
+    #subproblem_modifier = identity,
+    #subsolver_max_eval = max_eval,
+    #subsolver_kwargs = Dict(:max_cgiter => nlp.meta.nvar),
+  ),
+  :fps => nlp -> fps_solve(
+    nlp,
+    nlp.meta.x0,
+    σ_0 = 100.,
+    max_iter = typemax(Int64),
+    max_time = max_time,
+    max_eval = typemax(Int64),
+    atol = tol,
+    rtol = tol,
+  )
+)
+stats = bmark_solvers(solvers, cutest_problems)
+
+using JLD2
+@save "ipopt_dcildl_percival_fps_$(string(length(problems))).jld2" stats
---
+++ cutest_benchmark_percival_ipopt/figures.jl
@@ -0,0 +1,23 @@
+using Pkg; Pkg.activate(".")
+using JLD2, Plots, SolverBenchmark #, SolverCore
+
+@load "ipopt_dcildl_percival_fps_45.jld2" stats
+solved(df) = (df.status .== :first_order)
+
+for solver in keys(stats)
+  # Number of problems solved by ipopt
+  @show size(stats[solver][solved(stats[solver]), [:name]], 1)
+end
+
+costs = [
+  df -> .!solved(df) * Inf + df.elapsed_time,
+  df -> .!solved(df) * Inf + df.neval_obj + df.neval_cons,
+]
+costnames = ["Time", "Evaluations of obj + cons"]
+p = profile_solvers(stats, costs, costnames)
+png(p, "ipopt_dcildl_percival_fps_45")
+# Plots.svg(p, "ipopt_dcildl_82")
+
+open("stats_fps.dat", "w") do io
+  print(io, stats[:fps][!, [:name, :nvar, :ncon, :status, :objective, :elapsed_time, :iter, :primal_feas, :dual_feas]])
+end
