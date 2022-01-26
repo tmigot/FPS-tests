9f73436b1abba3b50b93b37898f9ae82997754b2
tmigot <tangi.migot@gmail.com>
2022-01-25 20:08:11 -0500

    add figures for n=300 K-FPS

--- cutest_benchmark_percivalKN_ipopt/figures.jl
+++ cutest_benchmark_percivalKN_ipopt/figures.jl
@@ -1,7 +1,7 @@
 using Pkg; Pkg.activate(".")
 using JLD2, Plots, SolverBenchmark #, SolverCore
 
-@load "ipopt_dcildl_percival_fps_45.jld2" stats
+@load "ipopt_dcildl_percival_fpsK_45.jld2" stats
 solved(df) = (df.status .== :first_order)
 
 for solver in keys(stats)
@@ -15,9 +15,9 @@ costs = [
 ]
 costnames = ["Time", "Evaluations of obj + cons"]
 p = profile_solvers(stats, costs, costnames)
-png(p, "ipopt_dcildl_percival_fps_45")
+png(p, "ipopt_dcildl_percival_fpsK_45")
 # Plots.svg(p, "ipopt_dcildl_82")
 
-open("stats_fps.dat", "w") do io
+open("stats_fpsK_45.dat", "w") do io
   print(io, stats[:fps][!, [:name, :nvar, :ncon, :status, :objective, :elapsed_time, :iter, :primal_feas, :dual_feas]])
 end
